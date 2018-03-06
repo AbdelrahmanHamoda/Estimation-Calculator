13299b3a376a0209b7d6a17debd34e1ee4e611f4
AbdelrahmanHamoda <35891980+AbdelrahmanHamoda@users.noreply.github.com>
2018-03-06 22:49:19 +0200

    new ideas

--- script.py
+++ script.py
@@ -27,17 +27,22 @@ class MainApp(QMainWindow, UI_File):
         self.risk2.setVisible(False)
         self.risk3.setVisible(False)
         self.risk4.setVisible(False)
+        self.multi_label.setVisible(False)
 
     def actions(self):
         self.reset_bt.clicked.connect(self.reset)
         self.clear_bt.clicked.connect(self.clear)
         self.calculate_bt.clicked.connect(self.calculate)
-        self.risk_yes.toggled.connect(self.ui_update)
+        # self.risk_yes.toggled.connect(self.ui_update)
         self.p1call.toggled.connect(self.ui_update)
         self.p2call.toggled.connect(self.ui_update)
         self.p3call.toggled.connect(self.ui_update)
         self.p4call.toggled.connect(self.ui_update)
-        self.double_risk.toggled.connect(self.ui_update)
+        self.p1call.toggled.connect(self.update)
+        self.p2call.toggled.connect(self.update)
+        self.p3call.toggled.connect(self.update)
+        self.p4call.toggled.connect(self.update)
+        # self.double_risk.toggled.connect(self.ui_update)
         self.p1edit.textChanged.connect(self.ui_update)
         self.p2edit.textChanged.connect(self.ui_update)
         self.p3edit.textChanged.connect(self.ui_update)
@@ -47,8 +52,97 @@ class MainApp(QMainWindow, UI_File):
         self.p3result.textChanged.connect(self.ui_update)
         self.p4result.textChanged.connect(self.ui_update)
 
+    def update(self):
+        p1call = self.p1edit.text()
+        p2call = self.p2edit.text()
+        p3call, p3test = self.p3edit.text(), self.p3edit.text()
+        p4call = self.p4edit.text()
+        calls = []
+
+        try:
+            p1call = int(self.p1edit.text())
+            calls.append(p1call)
+        except:
+            print('1this is dc or not valid')
+        try:
+            p2call = int(self.p2edit.text())
+            calls.append(p2call)
+        except:
+            print('2this is dc or not valid')
+        try:
+            p3call = int(self.p3edit.text())
+            calls.append(p3call)
+        except:
+            print(p3test)
+            if p3test == 'dc':
+                p3test = 0
+                calls.append(p3test)
+                print(p3test)
+            else:
+                QMessageBox.warning(self, 'error', 'not valid value')
+                return
+            print('3this is dc or not valid')
+        try:
+            p4call = int(self.p4edit.text())
+            calls.append(p4call)
+        except:
+            print('4this is dc or not valid')
+
+        if self.p1call.isChecked():
+            if p1call == max(calls):
+                if p4call == 'dc':
+                    if p3call == 'dc':
+                        if p2call == 'dc':
+                            print('sbane5')
+                        else:
+                            # p2 test
+                            dif = abs(13 - p1call)
+                            print('difff', dif)
+                            print(p1call)
+                            print(p2call)
+                            print(p3call)
+                            print(p4call)
+                            if abs(dif - p2call == 2) or abs(dif - p2call == 3):
+                                print('1Risk')
+                            elif abs(dif - p2call == 1) or abs(dif - p2call == 0):
+                                print('1not risk')
+                            else:
+                                print('1D.Risk')
+                    else:
+                        # p3 test
+                        dif = abs(13 - (p1call + p2call))
+                        print('difff', dif)
+                        print(p1call)
+                        print(p2call)
+                        print(p3call)
+                        print(p4call)
+                        if abs(dif - p3call) == 3 or abs(dif - p3call) == 2:
+                            print('2Risk')
+                        elif abs(dif - p3call) == 1 or abs(dif - p3call) == 0:
+                            print('2no risk')
+                        else:
+                            print('2D.Risk')
+                else:
+                    # p4 test
+                    if type(p3test) == str:
+                        dif = abs(13 - (p1call + p3call + p2call))
+                    else:
+                        dif = abs(13 - (p1call + p3test + p2call))
+                    print('difff', dif)
+                    print(p1call)
+                    print(p2call)
+                    print(p3call)
+                    print(p4call)
+                    if abs(dif - p4call) == 3 or abs(dif - p4call) == 2:
+                        print('3Risk')
+                    elif abs(dif - p4call) == 1 or abs(dif - p4call) == 0:
+                        print('3not risk')
+                    else:
+                        print('3D.Risk')
+
     def ui_update(self):
         # risk and double risk
+        '''
         if self.double_risk.isChecked():
             self.risk1.setText('D.Risk')
             self.risk1.setGeometry(170, 60, 41, 16)
@@ -67,6 +161,7 @@ class MainApp(QMainWindow, UI_File):
             self.risk3.setGeometry(710, 60, 31, 16)
             self.risk4.setText('Risk')
             self.risk4.setGeometry(710, 230, 31, 16)
+
         # risk checked or not
         if self.risk_yes.isChecked() and self.p1risk.isChecked() and self.p1risk.isEnabled():
             self.risk1.setVisible(True)
@@ -84,6 +179,8 @@ class MainApp(QMainWindow, UI_File):
             self.risk4.setVisible(True)
         else:
             self.risk4.setVisible(False)
+            '''
+
         # with checked or not
         if self.p1call.isChecked() and (not self.p1with.isEnabled()):
             self.p1with.setChecked(False)
@@ -101,6 +198,12 @@ class MainApp(QMainWindow, UI_File):
         else:
             self.calculate_bt.setEnabled(True)
 
+        if self.p1edit.text() == '' or self.p2edit.text() == '' or self.p3edit.text() == '' or \
+                self.p4edit.text() == '':
+            self.call_group.setEnabled(False)
+        else:
+            self.call_group.setEnabled(True)
+
     def calculate(self):
         p1in = self.p1edit.text()
         p2in = self.p2edit.text()
@@ -114,6 +217,7 @@ class MainApp(QMainWindow, UI_File):
         caller = []
         ls_players = ['p1', 'p2', 'p3', 'p4']
         p1, p2, p3, p4 = [], [], [], []
+        winners, losers = [], []
         # checking if input and result values are valid or not
         if not error_input(p1in):
             QMessageBox.warning(self, 'player1 error', 'input must be positive number or dc only')
@@ -126,6 +230,7 @@ class MainApp(QMainWindow, UI_File):
                 ls_sum_in.append(p1in)
             else:
                 ls_sum_in.append(0)
+
         if not error_input(p2in):
             QMessageBox.warning(self, 'player2 error', 'input must be positive number or dc only')
             p2in = ''
@@ -137,6 +242,7 @@ class MainApp(QMainWindow, UI_File):
                 ls_sum_in.append(p2in)
             else:
                 ls_sum_in.append(0)
+
         if not error_input(p3in):
             QMessageBox.warning(self, 'player3 error', 'input must be positive number or dc only')
             p3in = ''
@@ -148,6 +254,7 @@ class MainApp(QMainWindow, UI_File):
                 ls_sum_in.append(p3in)
             else:
                 ls_sum_in.append(0)
+
         if not error_input(p4in):
             QMessageBox.warning(self, 'player4 error', 'input must be positive number or dc only')
             p4in = ''
@@ -159,6 +266,7 @@ class MainApp(QMainWindow, UI_File):
                 ls_sum_in.append(p4in)
             else:
                 ls_sum_in.append(0)
+
         if not error_input(p1result):
             QMessageBox.warning(self, 'result1 error', 'result must be positive number or dc only')
             p1result = ''
@@ -167,6 +275,10 @@ class MainApp(QMainWindow, UI_File):
         else:
             if not p1result == 'dc':
                 p1result = int(p1result)
+                p1.append(p1result)
+            else:
+                p1.append(p1result)
+
         if not error_input(p2result):
             QMessageBox.warning(self, 'result2 error', 'result must be positive number or dc only')
             p2result = ''
@@ -175,6 +287,10 @@ class MainApp(QMainWindow, UI_File):
         else:
             if not p2result == 'dc':
                 p2result = int(p2result)
+                p2.append(p2result)
+            else:
+                p2.append(p2result)
+
         if not error_input(p3result):
             QMessageBox.warning(self, 'result3 error', 'result must be positive number or dc only')
             p3result = ''
@@ -183,6 +299,10 @@ class MainApp(QMainWindow, UI_File):
         else:
             if not p3result == 'dc':
                 p3result = int(p3result)
+                p3.append(p3result)
+            else:
+                p3.append(p3result)
+
         if not error_input(p4result):
             QMessageBox.warning(self, 'result4 error', 'result must be positive number or dc only')
             p4result = ''
@@ -191,9 +311,13 @@ class MainApp(QMainWindow, UI_File):
         else:
             if not p4result == 'dc':
                 p4result = int(p4result)
+                p4.append(p4result)
+            else:
+                p4.append(p4result)
+
         # No more two Dash calls
         if p1in == 'dc' and p2in == 'dc' and p3in == 'dc':
-            QMessageBox.warning(self,'error', 'no more two dash calls are allowed')
+            QMessageBox.warning(self, 'error', 'no more two dash calls are allowed')
             return
         elif p1in == 'dc' and p2in == 'dc' and p4in == 'dc':
             QMessageBox.warning(self, 'error', 'no more two dash calls are allowed')
@@ -206,27 +330,27 @@ class MainApp(QMainWindow, UI_File):
             return
         # doesn't accept dc in result if input not dc
         if not result_dash_call(p1in, p1result):
-            QMessageBox.warning(self,'result 1 error', 'result must be an number not dc')
+            QMessageBox.warning(self, 'result 1 error', 'result must be an number not dc')
             self.p1result.setText('')
             return
         if not result_dash_call(p2in, p2result):
-            QMessageBox.warning(self,'result 2 error', 'result must be an number not dc')
+            QMessageBox.warning(self, 'result 2 error', 'result must be an number not dc')
             self.p2result.setText('')
             return
         if not result_dash_call(p3in, p3result):
-            QMessageBox.warning(self,'result 3 error', 'result must be an number not dc')
+            QMessageBox.warning(self, 'result 3 error', 'result must be an number not dc')
             self.p3result.setText('')
             return
         if not result_dash_call(p4in, p4result):
-            QMessageBox.warning(self,'result 4 error', 'result must be an number not dc')
+            QMessageBox.warning(self, 'result 4 error', 'result must be an number not dc')
             self.p4result.setText('')
             return
 
         # make sure that total calls aren't = 13
         if sum(ls_sum_in) == 13:
-            QMessageBox.warning(self,'Call error', 'total calls must be lower or greater than 13')
+            QMessageBox.warning(self, 'Call error', 'total calls must be lower or greater than 13')
             return
-        else: # determining game is over or under
+        else:  # determining game is over or under
             if sum(ls_sum_in) > 13:
                 over = True
                 under = False
@@ -234,41 +358,37 @@ class MainApp(QMainWindow, UI_File):
                 over = False
                 under = True
 
+        # to determine the right caller
         call = max(ls_sum_in)
         for x, y in zip(ls_sum_in, ls_players):
             if x == call:
                 caller.append(y)
 
         if self.p1call.isChecked() and 'p1' in caller:
-            print('call correct')
             p1.append('call')
         elif self.p1call.isChecked():
-            QMessageBox.warning(self,'call error','this player is not allowed to get call')
+            QMessageBox.warning(self, 'call error', 'this player is not allowed to get call')
             return
 
         if self.p2call.isChecked() and 'p2' in caller:
-            print('call correct')
             p2.append('call')
         elif self.p2call.isChecked():
             QMessageBox.warning(self, 'call error', 'this player is not allowed to get call')
             return
 
         if self.p3call.isChecked() and 'p3' in caller:
-            print('call correct')
             p3.append('call')
         elif self.p3call.isChecked():
             QMessageBox.warning(self, 'call error', 'this player is not allowed to get call')
             return
 
         if self.p4call.isChecked() and 'p4' in caller:
-            print('call correct')
             p4.append('call')
         elif self.p4call.isChecked():
             QMessageBox.warning(self, 'call error', 'this player is not allowed to get call')
             return
 
         # determining who are winners and losers
-        winners, losers = [], []
         if win_lose(p1in, p1result):
             winners.append('p1')
         else:
@@ -282,37 +402,107 @@ class MainApp(QMainWindow, UI_File):
         else:
             losers.append('p3')
         if win_lose(p4in, p4result):
-             winners.append('p4')
+            winners.append('p4')
         else:
             losers.append('p4')
 
-            # only win or only lose
-            if only_win_lose(winners, losers):
-                if len(winners)<2 :
-                    print(winners[0])
-                else:
-                    print(losers[0])
+        # only win or only lose
+        if only_win_lose(winners, losers) or len(winners) == 2:
+            if len(winners) < 2 and 'p1' in winners:
+                p1.append('only win')
+            elif len(winners) < 2 and 'p2' in winners:
+                p2.append('only win')
+            elif len(winners) < 2 and 'p3' in winners:
+                p3.append('only win')
+            elif len(winners) < 2 and 'p4' in winners:
+                p4.append('only win')
+            elif len(losers) < 2 and 'p1' in losers:
+                p1.append('only lose')
+            elif len(losers) < 2 and 'p2' in losers:
+                p2.append('only lose')
+            elif len(losers) < 2 and 'p3' in losers:
+                p3.append('only lose')
+            elif len(losers) < 2 and 'p4' in losers:
+                p4.append('only lose')
+        elif len(winners) == 4:
+            QMessageBox.warning(self, 'error', "all players couldn't win in row")
+            return
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setText("No winners, multiplication needed, "
+                        "do you want to enable multiplication in the next round?")
+            msg.setWindowTitle("error")
+            msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+            retrieval = msg.exec_()
+            if retrieval == QMessageBox.Yes:
+                self.Yes_bt()
             else:
-                msg = QMessageBox()
-                msg.setIcon(QMessageBox.Warning)
-                msg.setText("No winners, multiplication needed, do you want to enable multiplication in the next round?")
-                msg.setWindowTitle("error")
-                msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-                retreval = msg.exec_()
-                if retreval == QMessageBox.Yes:
-                    self.Yes_bt()
-                else:
-                    self.No_bt()
-            # calculation logic starts here
+                self.No_bt()
+            return
 
+        # knowing who in Risk
+        '''
+        if self.risk_yes.isChecked() and self.risk1.isVisible():
+            if self.risk1.text() == 'Risk':
+                p1.append('risk')
+            else:
+                p1.append('D.Risk')
+    
+        if self.risk_yes.isChecked() and self.risk2.isVisible():
+            if self.risk2.text() == 'Risk':
+                p2.append('risk')
+            else:
+                p2.append('D.Risk')
+    
+        if self.risk_yes.isChecked() and self.risk3.isVisible():
+            if self.risk3.text() == 'Risk':
+                p3.append('risk')
+            else:
+                p3.append('D.Risk')
+    
+        if self.risk_yes.isChecked() and self.risk4.isVisible():
+            if self.risk4.text() == 'Risk':
+                p4.append('risk')
+            else:
+                p4.append('D.Risk')
+        '''
+        if self.p1with.isChecked():
+            if p1in == call:
+                p1.append('with')
+            else:
+                QMessageBox.warning(self, 'error', "this player can't attempt with")
+                self.p1with.setChecked(False)
+                return
 
-            p1.clear()
-            p2.clear()
-            p3.clear()
-            p4.clear()
-            ls_sum_in.clear()
-            ls_players.clear()
-            self.multi_no.setChecked(False)
+        if self.p2with.isChecked():
+            if p2in == call:
+                p2.append('with')
+            else:
+                QMessageBox.warning(self, 'error', "this player can't attempt with")
+                self.p2with.setChecked(False)
+                return
+
+        if self.p3with.isChecked():
+            if p3in == call:
+                p3.append('with')
+            else:
+                QMessageBox.warning(self, 'error', "this player can't attempt with")
+                self.p3with.setChecked(False)
+                return
+
+        if self.p4with.isChecked():
+            if p4in == call:
+                p4.append('with')
+            else:
+                QMessageBox.warning(self, 'error', "this player can't attempt with")
+                self.p4with.setChecked(False)
+                return
+
+        # calculation logic starts here
+
+        print(p1, p2, p3, p4)
+        self.multi_no.setChecked(False)  # final step
 
     def Yes_bt(self):
         self.multi_yes.setChecked(True)
@@ -329,6 +519,7 @@ class MainApp(QMainWindow, UI_File):
         self.p2result.setText('')
         self.p3result.setText('')
         self.p4result.setText('')
+        self.multi_no.setChecked(False)
 
     def reset(self):
         self.p1sc.setText('score')
@@ -343,6 +534,7 @@ class MainApp(QMainWindow, UI_File):
         self.p2result.setText('')
         self.p3result.setText('')
         self.p4result.setText('')
+        self.multi_no.setChecked(False)
 
 
 def only_win_lose(winners, losers):
